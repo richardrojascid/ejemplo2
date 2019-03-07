1428387aa55b43f76d01493b3ad3318ac76d0193
cetecom <cetecom@LCT50404>
2019-03-06 22:12:47 -0300

    foreach ejemplo

--- index.php
+++ index.php
@@ -15,7 +15,27 @@ and open the template in the editor.
         <?php
         $saludo="Mundo variables";
         echo "Hola Mundo 2";
+        echo "<br>";
         echo "Hola ".$saludo;
+        ?>
+        <br>
+        <?php
+            $meses["enero"]="verano";
+            $meses["febrero"]="verano";
+            $meses["marzo"]="verano/otoño";
+            $meses["abril"]="otoño";
+            $meses["mayo"]="otoño/invierno";
+            $meses["junio"]="invierno";
+            $meses["julio"]="invierno";
+            $meses["agosto"]="invierno";
+            $meses["septiembre"]="invierno/primavera";
+            $meses["octubre"]="primavera";
+            $meses["noviembre"]="primavera";
+            $meses["diciembre"]="primavera/verano";
+            
+            foreach($meses as $mes){
+                echo  $mes."<br>";
+            }
         ?>
         
         <br>
